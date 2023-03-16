d15b32341ed2ce9ecfba1581974c9e89335ed11e
Samuel Starko <00109219@uwa.edu.au>
2023-03-16 15:53:19 +0800

    16/03/2023

--- Analyses/BleachingCode.R
+++ Analyses/BleachingCode.R
@@ -1,41 +1,5 @@
 #Bleaching comparison
 
-#Import data
-bleach_2016a <- read_excel("./Data/Coral_tracking/Mortality_and_bleaching.xlsx", sheet = "2016a")
-
-bleach_2016a <- filter(bleach_2016a, Lineage!="NA") %>% filter(Lineage!="Unknown")
-
-sum2016_1<- table(bleach_2016a$Lineage, bleach_2016a$bleach2) %>% prop_row() %>% as.data.frame()
-
-sum2016_1$Var1 <- sum2016_1$Var1 %>% factor(ordered = TRUE, levels =  c("Red","Blue",  "Pale"))
-
-plot16<-ggplot(sum2016_1, aes(x = Var1, y = Freq, fill = Var2, color = Var2))+
-  geom_col()+
-  scale_fill_manual(values = c("White", "lightgrey", "darkgrey", "black"))+theme_cowplot()+
-  scale_color_manual(values = c("black", "black","black","black"))
-
-bleach_2015c <- read_excel("./Data/Coral_tracking/Mortality_and_bleaching.xlsx", sheet = "2015c")
-
-bleach_2015c <- bleach_2015c %>% filter(Lineage!="NA") %>% filter(Lineage!="Unknown")
-
-sum2015_1<- table(bleach_2015c$Lineage, bleach_2015c$bleaching2) %>% prop_row() %>% as.data.frame()
-
-sum2015_1$Var1 <- sum2015_1$Var1 %>% factor(ordered = TRUE, levels = c("Red","Blue",  "Pale"))
-
-plot15<- ggplot(sum2015_1, aes(x = Var1, y = Freq, fill = Var2, color = Var2))+
-  geom_col()+
-  scale_fill_manual(values = c("White", "lightgrey", "darkgrey", "black"))+theme_cowplot()+
-  scale_color_manual(values = c("black", "black","black","black"))
-
-
-bleach_2016a_2 <- bleach_2016a %>% filter(Lineage!="NA") %>% filter(Lineage!="Unknown")
-
-vglm(factor(bleach2, ordered = TRUE) ~ Lineage, data = bleach_2016a_2, family = propodds) %>% anova()
-
-vglm(factor(bleaching2, ordered = TRUE) ~ Lineage, data = bleach_2015c, family = propodds) %>% anova() 
-
-plot_grid(plot15, plot16)
-
 ##McClanahan categories
 
 #Import data
@@ -80,7 +44,7 @@ vglm(factor(bleach_2015c$Bleaching_McClanahan5, ordered = TRUE) ~ Lineage, data 
 plot_grid(plot15_1, plot16_1)
 
 
-##kt categories
+##Baum categories
 
 #Import data
 bleach_2016a <- read_excel("./Data/Coral_tracking/Mortality_and_bleaching.xlsx", sheet = "2016a")
